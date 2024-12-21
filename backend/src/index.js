4536547f6e09f30979116f8f4fb14cbc15d2019d
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-22 00:40:30 +0530

    server deploy #1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 import app from "./app.js";
 import { connectDB } from "./db/connect.js";
 
-app.get("/", (req, res) => res.json("Hello Welcome"));
+const startServer = () => {
+  app.get("/", (req, res) => res.json("Hello Welcome"));
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(` Server is listening to port ${process.env.PORT || 3000}`);
-    });
-  })
-  .catch((err) => console.log("MongoDb connnection failed: ", err));
+  connectDB()
+    .then(() => {
+      app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is listening to port ${process.env.PORT || 3000}`);
+      });
+    })
+    .catch((err) => console.log("MongoDb connection failed: ", err));
+};
+
+startServer();
+
+export default startServer;
