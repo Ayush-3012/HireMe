49f6ef402670034c7510d0c610d93db9d404fbd9
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-22 02:18:15 +0530

    server deploy #10

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,17 @@
 import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
 import connectDB from "./db/connect.js";
 
-dotenv.config();
-
 const startServer = () => {
-  app.get("/", (req, res) => res.json("Hello Welcome"));
-
-  // connectDB()
-  //   .then(() => {
-  //     app.listen(process.env.PORT, () => {
-  //       console.log(` Server is listening to port ${process.env.PORT}`);
-  //     });
-  //   })
-  //   .catch((err) => console.log("MongoDb connection failed: ", err));
+  connectDB()
+    .then(() => {
+      app.listen(process.env.PORT, () => {
+        console.log(` Server is listening to port ${process.env.PORT}`);
+      });
+    })
+    .catch((err) => console.log("MongoDb connection failed: ", err));
 };
 
 startServer();
