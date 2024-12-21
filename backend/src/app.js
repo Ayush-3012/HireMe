95998d09bcf31c098b64070ed71fa03bc2232d3d
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-22 00:55:49 +0530

    server deploy #2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,12 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-dotenv.config();
-
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.get("/", (req, res) => res.json("Hello Welcome"));
+
+app.use(cors({ origin: process.env.CORS, credentials: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser(process.env.COOKIE_SECRET));
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
+import dotenv from "dotenv";
 import app from "./app.js";
 import { connectDB } from "./db/connect.js";
 
-const startServer = () => {
-  app.get("/", (req, res) => res.json("Hello Welcome"));
+dotenv.config({
+  path: "./env",
+});
 
+const startServer = () => {
   connectDB()
     .then(() => {
       app.listen(process.env.PORT || 3000, () => {
-        console.log(`Server is listening to port ${process.env.PORT || 3000}`);
+        console.log(` Server is listening to port ${process.env.PORT || 3000}`);
       });
     })
     .catch((err) => console.log("MongoDb connection failed: ", err));
