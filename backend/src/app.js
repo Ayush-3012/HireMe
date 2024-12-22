d9d55812341f4518c16a51277b49ec4d15c23938
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-22 13:38:15 +0530

    server deploye #11

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.get("/", (req, res) => res.json("Hello Welcome"));
 
-app.use(cors({ origin: process.env.CORS, credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
