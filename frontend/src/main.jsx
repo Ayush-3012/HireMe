258d8c2e5c69d2e947569a1df3bd15e0c588653e
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-22 16:52:46 +0530

    frontend deploy #1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import App from "./App.jsx";
-import "./index.css";
+import "./Index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
