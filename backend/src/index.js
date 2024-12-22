79ead4033ce6c15dda4c35cb7504a425b6df6ee1
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-22 16:16:11 +0530

    server deploy #19

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./db/connect.js";
 
 import app from "./app.js";
 app.get("/", (req, res) => res.send("Hello, Welcome!"));
 
-connectDb()
+connectDB()
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(` Server is listening to port ${process.env.PORT}`);
